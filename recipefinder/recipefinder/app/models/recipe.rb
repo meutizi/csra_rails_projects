cad3081379724501a219efd24550b8e9975a975f
Mark Eutizi <meutizi@gmail.com>
2016-01-13 12:25:09 -0500

    add .rspec

--- recipefinder/recipefinder/app/models/recipe.rb
+++ recipefinder/recipefinder/app/models/recipe.rb
@@ -1,8 +1,10 @@
 class Recipe
   include HTTParty
   #1f8a5aa4ec496312b377885e653bcc12
-  key_value = '1f8a5aa4ec496312b377885e653bcc12'#ENV[’FOOD2FORK_KEY’]
-  hostport = 'www.food2fork.com' #ENV[’FOOD2FORK_SERVER_AND_PORT’] || ’www.food2fork.com’
+  #echo 'export FOOD2FORK_SERVER_AND_PORT=www.food2fork.com' >> ~/.bashrc
+    #source ~/.bashrc
+  key_value = ENV[’FOOD2FORK_KEY’]
+  hostport = ENV[’FOOD2FORK_SERVER_AND_PORT’] || ’www.food2fork.com’
   base_uri "http://#{hostport}/api"
 
   default_params key: key_value
